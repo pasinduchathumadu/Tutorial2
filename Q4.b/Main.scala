6e1f64fd79dc8d047634cf422cc05096b344971b
Pasindu <pasindugura123@gmail.com>
2022-07-10 14:34:12 +0530

    This file has a Q4.b

---
+++ Q4.b/Main.scala
@@ -0,0 +1,27 @@
+
+//------------------------>Q4.b
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("The highest profit:" + profit(15))
+  }
+  def attendees(price:Int):Int={
+    return 120+(15-price)/5*20
+  }
+  def revenue(price:Int):Int={
+    return attendees(price)*price
+  }
+  def cost(price:Int):Int={
+    return 500+3*attendees(price)
+  }
+  def profit(price:Int):Int={
+    return revenue(price)-cost(price)
+  }
+    /* Ticket price is 25 ---- profit=1240
+       Ticket price is 30 ---- profit=1120
+       Ticket price is 35 ---- profit=780
+       Ticket price is 40 ---- profit=240
+
+       Ticket price is 20 ---- profit=1200
+       Ticket price is 15 ---- profit=940
+     */
+}
